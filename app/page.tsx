3f9ae592556a93f44bb04fa410900cbbf72175a5
AlexAirCG <alexairggg@yandex.ru>
2024-06-28 10:40:58 +0300

    28.06: add bg-image

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>my portfolio</h1>
+    <main className="flex items-center h-screen relative bg-cover bg-[url('/assets/bg-explosion.png')]">
+      <div className="absolute right-10 md:right-40 bottom-0 z-[10]/">
+        <Image
+          src="/assets/MoustacheMan.png"
+          alt="MoustachMan"
+          width={560}
+          height={560}
+          className="w-[300px] h-[300px] z-[1] md:w-[560px] md:h-[560px]"
+        />
+      </div>
+      <Image
+        src="/assets/top-left-img.png"
+        alt="paint"
+        width={230}
+        height={230}
+        className="absolute left-0 top-0"
+      />
     </main>
   );
 }
