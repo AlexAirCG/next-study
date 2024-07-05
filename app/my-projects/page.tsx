91f0ddd84b04b2a003bda645db719d1dcc5fb823
AlexAirCG <alexairggg@yandex.ru>
2024-07-05 10:40:42 +0300

    05.07-add Nav Sid Tran

---
+++ app/my-projects/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>My Project</div>;
+};
+
+export default Page;
--- tests/page.spec.ts
+++ tests/page.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
+  await page.locator("body").click();
   await page.goto("http://localhost:3000/");
-  await page.getByText("Lorem ipsum dolor sit amet").click();
+  await page.getByRole("link").nth(1).click();
+  await page.getByRole("link").nth(2).click();
+  await page.getByRole("link").first().click();
 });
